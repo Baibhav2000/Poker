e1e11e1ffc556c631f354addb131ba8dc2d1c664
Baibhav Chakraborty <baibhav.chakrabortybc2000@gmail.com>
2024-02-20 08:45:49 +0530

    Implementation of Poker Card
    Signed-off-by: Baibhav Chakraborty <baibhav.chakrabortybc2000@gmail.com>

---
+++ include/card.h
@@ -0,0 +1,71 @@
+#ifndef CARD_H
+#define CARD_H
+
+typedef enum CardRank{
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN,
+	JACK,
+	QUEEN,
+	KING,
+	ACE
+}CardRank;
+
+class Card{
+	private:
+		char cardValue;
+		char cardSuit;
+		CardRank cardRank;
+
+	public:
+		/*******************************************************
+		 * @brief Sets Card Salue
+		 * 
+		 * @param[in] cardValue 
+		********************************************************/
+		void setCardValue(char cardValue);
+
+
+		/*******************************************************
+		 * @brief Fetches Card Value
+		 * 
+		 * @return Card Value
+		********************************************************/
+		char getCardValue();
+
+
+		/*******************************************************
+		 * @brief Sets Card Suit
+		 * 
+		 * @param[in] cardSuit
+		********************************************************/
+		void setCardSuit(char cardSuit);
+
+
+		/*******************************************************
+		 * @brief Fetches Card Suit
+		 * 
+		 * @return Card Suit
+		********************************************************/
+		char getCardSuit();
+
+
+		/*******************************************************
+		 * @brief Sets Card Rank
+		********************************************************/
+		void setCardRank();
+
+
+		/********************************************************
+		 * @brief Fetches Card Rank
+		*********************************************************/
+		CardRank getCardRank();
+};
+
+#endif
---
+++ src/card.cpp
@@ -0,0 +1,80 @@
+#include "card.h"
+
+void Card::setCardValue(char cardValue){
+	this->cardValue = cardValue;
+}
+
+char Card::getCardValue(){
+	return this->cardValue;
+}
+
+void Card::setCardSuit(char cardSuit){
+	this->cardSuit = cardSuit;
+}
+
+char Card::getCardSuit(){
+	return this->cardSuit;
+}
+
+void Card::setCardRank(){
+
+	switch(this->cardRank){
+
+		case '2':
+			this->cardRank = TWO;
+			break;
+		
+		case '3':
+			this->cardRank = THREE;
+			break;
+		
+		case '4':
+			this->cardRank = FOUR;
+			break;
+		
+		case '5':
+			this->cardRank = FIVE;
+			break;
+
+		case '6':
+			this->cardRank = SIX;
+			break;
+
+		case '7':
+			this->cardRank = SEVEN;
+			break;
+
+		case '8':
+			this->cardRank = EIGHT;
+			break;
+
+		case '9':
+			this->cardRank = NINE;
+			break;
+
+		case 'T':
+			this->cardRank = TEN;
+			break;
+
+		case 'J':
+			this->cardRank = JACK;
+			break;
+
+		case 'Q':
+			this->cardRank = QUEEN;
+			break;
+
+		case 'K':
+			this->cardRank = KING;
+			break;
+
+		case 'A':
+			this->cardRank = ACE;
+			break;
+		
+	}
+}
+
+CardRank Card::getCardRank(){
+	return this->cardRank;
+}
